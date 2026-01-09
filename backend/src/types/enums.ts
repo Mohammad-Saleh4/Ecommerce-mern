8be4374ad721aac69b1a1da424715e049404a628
Mohammad Saleh <mohammad.saleh.3004@gmail.com>
2026-01-10 00:56:15 +0200

    Add enums for UserRole and OrderStatus in enums.ts

---
+++ backend/src/types/enums.ts
@@ -0,0 +1,13 @@
+export enum UserRole {
+  USER = 'USER',
+  ADMIN = 'ADMIN',
+}
+
+export enum OrderStatus {
+  PENDING = 'PENDING',
+  CONFIRMED = 'CONFIRMED',
+  SHIPPED = 'SHIPPED',
+  DELIVERED = 'DELIVERED',
+  CANCELED = 'CANCELED',
+}
+
